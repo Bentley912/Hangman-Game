d94aec9aad3464c6ac945cb74e489836f3794d82
Patrick <pbentley07@gmail.com>
2017-02-12 18:53:30 -0500

    Added code for Word Spaces

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,3 +13,11 @@ var tries = 15;
 
 var computerGuess = words[Math.floor(Math.random() * words.length)];
 console.log(computerGuess);
+
+      for (i=1;i <= computerGuess.length;i++ ){
+
+          var html = "_";
+          //document.querySelector(".container");
+          //document.querySelector(".main-content");
+          document.querySelector("#current").innerHTML = html;
+      }
