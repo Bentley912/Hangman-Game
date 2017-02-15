2689f3565aacccb916ab3a868b79478115d83346
Patrick <pbentley07@gmail.com>
2017-02-14 23:12:27 -0500

    Added Key Events and Jquery
    Added key events and empty arrays for words and user guesses

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,28 +1,42 @@
 
 
-// set array for words to be guessed
-var words = ["Wakanda", "Batman", "Shadow", "Manhattan", "Xavier", "Wolverine", "Spawn"];
 
-//set user guesses and wins
-var user = {
-  guess: [],
+//sets initial array for word library and math for
+  var words = ["Wakanda", "Batman", "Shadow", "Manhattan", "Xavier", "Wolverine", "Spawn"];
+    var computerGuess = words[Math.floor(Math.random() * words.length)];
+    //empty array for selected word letter split
+    var letters = [];
+  //set user guesses and wins
+  var user = {
+    guesses: [],
+    attempts: 15,
+  };
+
+  var wins = 0;
+//array for dashes that equal length of word
+  var dashes = [];
+
+//creates mechanisms for key event
+document.onkeyup = function(event){
+// set array for words to be guessed
+
+          var userGuess = event.key;
+          console.log(computerGuess);
+          console.log(userGuess);
+          user.guesses.push(userGuess);
+          console.log(user.guesses);
+          user.attempts--;
+          console.log(user.attempts);
 };
 
-var wins = 0;
-var tries = 15;
+//pushes dashes to array to display dashes that represent hidden letters in selected word
+                for (i=1;i <= computerGuess.length;i++ ){
 
-var dashes = [];
+                    dashes.push("_");
+                    //document.querySelector(".container");
+                    //document.querySelector(".main-content");
 
-var computerGuess = words[Math.floor(Math.random() * words.length)];
-console.log(computerGuess);
-
-      for (i=1;i <= computerGuess.length;i++ ){
-
-          dashes.push("_");
-          //document.querySelector(".container");
-          //document.querySelector(".main-content");
-
-      }
-
-document.querySelector("#current").innerHTML = dashes.join("");
-document.querySelector("#counter").innerHTML = wins;
+                }
+//displays content on page
+          document.querySelector("#current").innerHTML = dashes.join("");
+          document.querySelector("#counter").innerHTML = wins;
