e88ed9e8dc39967cc5518f2017343f59eecd3c99
Patrick <pbentley07@gmail.com>
2017-02-17 09:39:28 -0500

    Made edites to js

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,27 +3,25 @@
 
 //sets initial array for word library and math for
   var words = ["Wakanda", "Batman", "Shadow", "Manhattan", "Xavier", "Wolverine", "Spawn"];
-    var computerGuess = words[Math.floor(Math.random() * words.length)];
+  var computerGuess = words[Math.floor(Math.random() * words.length)];
     //empty array for selected word letter split
-    var letters = [];
+  var letters = [];
     //Split word into indvidual letters
-    var split = computerGuess.split("");
+  var split = computerGuess.split("");
     letters.push(split);
-    console.log(letters);
+
   //set user guesses and wins
   var user = {
     guesses: [],
     attempts: 15,
   };
 
-
-
   var wins = 0;
 //array for dashes that equal length of word
   var dashes = [];
 
 //creates mechanisms for key event
-document.onkeyup = function(event){
+    document.onkeyup = function(event){
 // set array for words to be guessed
 
           var userGuess = event.key;
@@ -36,18 +34,26 @@ document.onkeyup = function(event){
           document.querySelector("#guesses").innerHTML = user.guesses;
           document.querySelector("#attempts").innerHTML = user.attempts;
 
-          //get indexes where selection exists in randomArray
-							function getAllIndexes(arr, val) {
-							var index = [];
-							for (i = 0; i < arr.length; i++) {
-									if (arr[i]===val)
-									index.push(val);
-							 return(index);
-							}
-            }    
-              var indexes = getAllIndexes(letters, userGuess);
-							console.log(indexes);
-};
+          function getAllIndexes(arr,val){
+            var indexes = [];
+            for(i = 0;i<arr.length; i++){
+              if (arr[i] === val){
+                indexes.push(i);
+              }
+            }
+            return(indexes);
+          }
+          var indexes = getAllIndexes(split, userGuess);
+          console.log(indexes);
+    };
+
+//get indexes where selection exists in randomArray
+
+
+
+
+
+      console.log("letter", letters);
 
 //pushes dashes to array to display dashes that represent hidden letters in selected word
                 for (i=1;i <= computerGuess.length;i++ ){
